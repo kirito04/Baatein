9568004592d715a108d945f88985aac0c69cf72e
Yash <yashraj25.yr@gmail.com>
2021-03-29 00:25:56 +0530

    SplashScreen Corrected

--- app/src/main/java/com/example/baatein/SplashScreen.java
+++ app/src/main/java/com/example/baatein/SplashScreen.java
@@ -6,13 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class StartingWindow extends AppCompatActivity {
+public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_starting_window);
-
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
